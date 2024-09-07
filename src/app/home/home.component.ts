3fd16018a56a09bb7781976964b790850361f659
kephalosk <watchfromtheshore@gmail.com>
2024-09-07 16:13:02 +0200

    add home component and styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styles: ``
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
