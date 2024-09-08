7adf4e6b6b252d308f3f05b20e9776b9535adf86
kephalosk <watchfromtheshore@gmail.com>
2024-09-08 14:00:17 +0200

    add new list items

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,9 +6,16 @@ import {HousingLocation} from "../housing-location";
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!2
-    </p>
+    <section class="listing">
+      <img
+          class="listing-photo"
+          [src]="housingLocation.photo"
+          alt="Exterior photo of {{ housingLocation.name }}"
+          crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
   styles: ``,
   styleUrls: ['housing-location.components.css']
